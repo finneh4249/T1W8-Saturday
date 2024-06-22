09521b0bc509142dc9ae1715169ad925c834f13b
Ethan Cornwill <ethancornwill@outlook.com>
2024-06-22 10:24:39 +1000

    0 division fix bc i'm dumb

--- test_project/tests/test_calculator.py
+++ test_project/tests/test_calculator.py
@@ -59,7 +59,7 @@ def test_multiply(a, b, result):
 @pytest.mark.parametrize(
     "a, b, result",
     [
-        (0, 0, 1),
+        (0, 0, ValueError("Cannot divide by zero")),
         (1, 1, 1),
         (2, 2, 1),
         (3, 3, 1),
